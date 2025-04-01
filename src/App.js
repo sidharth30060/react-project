fb571fe51ed6fb7f32d1f2149c9e9f70237043ad
Sidharth Sharma <Sidharth.Sharma1@dell.com>
2025-04-01 08:41:26 +0000

    Update react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import V1 from './v1/index';
+import V2 from './v2/index';
+import V3 from './v3/index';
+import V4 from './v4/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/v1">V1</Link></li>
+            <li><Link to="/v2">V2</Link></li>
+            <li><Link to="/v3">V3</Link></li>
+            <li><Link to="/v4">V4</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/v1" element={<V1 />} />
+          <Route path="/v2" element={<V2 />} />
+          <Route path="/v3" element={<V3 />} />
+          <Route path="/v4" element={<V4 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
